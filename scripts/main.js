50cae6016e30aa6e3f1eacc0bf42f6c106397626
Kay McKelly <frecklestars@gmail.com>
2017-06-06 10:12:37 -0700

    Building example drag and drop with rough outline of what the finished product would look like

---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  var talks = Array.prototype.slice.call(document.querySelectorAll("div.category"));
+  var sessions = Array.prototype.slice.call(document.querySelectorAll(".session-talks"));
+  var containers = talks.concat(sessions);
+
+  dragula(containers).on('drop', function(el, target, source, sibling) {
+    if (target.classList.contains('session-talks')) {
+      console.log('added to a session');
+      console.dir(target);
+
+      // hide the drag & drop hint.
+      target.firstChild.data = '';
+    }
+
+    if (source.classList.contains('session-talks')) {
+      console.log('removed from a session');
+      // console.dir(source);
+
+      // Restore the drag & drop hint.
+      if (source.childNodes.length == 1) {
+        source.firstChild.data = 'Drag talks here';
+      }
+    }
+  });
+})
