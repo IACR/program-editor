ad6885b5a0e9216bdebc4480f37366b60e991e6d
Kay McKelly <frecklestars@gmail.com>
2017-06-10 09:33:07 -0700

    Input replacement working via selection by id of click event

--- scripts/editableFields.js
+++ scripts/editableFields.js
@@ -1,25 +1,33 @@
-function divClicked() {
+// function divClicked() {
     // get elements with class name session-title
     // change them to <input> onclick
     // make them not all change at once
-    var divHtml = document.getElementsByClassName('session-title');
-    var editableText = $("<input type='text' class='form-control' placholder='Type in your new title here' />");
-    editableText.val(divHtml);
-    $(divHtml).replaceWith(editableText);
-    editableText.focus();
-    // setup the blur event for this new textarea
-    editableText.blur(editableTextBlurred);
-}
-
-function editableTextBlurred() {
-    var html = $(this).val();
-    var viewableText = $("<div>");
-    viewableText.html(html);
-    $(this).replaceWith(viewableText);
-    // setup the click event for this new div
-    viewableText.click(divClicked);
-}
+//     var divHtml = document.getElementsByClassName('session-title');
+//     var editableText = $("<input type='text' class='form-control' placholder='Type in your new title here' />");
+//     editableText.val(divHtml);
+//     $(divHtml).replaceWith(editableText);
+//     editableText.focus();
+//     // setup the blur event for this new textarea
+//     editableText.blur(editableTextBlurred);
+// }
+//
+// function editableTextBlurred() {
+//     var html = $(this).val();
+//     var viewableText = $("<div>");
+//     viewableText.html(html);
+//     $(this).replaceWith(viewableText);
+//     // setup the click event for this new div
+//     viewableText.click(divClicked);
+// }
+//
+// $(document).ready(function() {
+//     $("h5").click(divClicked);
+// });
 
 $(document).ready(function() {
-    $("h5").click(divClicked);
+  $(".session-title").click(function() {
+    var clickity = $(this).attr("id");
+    $(this).replaceWith("<input type='text' class='form-control input-sm' placeholder='Enter new session name here' />");
+    //TODO: autofocus on the input box
+  });
 });
