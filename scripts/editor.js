e9a09a54da49689fe0fce6135d86361a9b69cf27
Kevin McCurley <github@digicrime.com>
2017-08-11 11:29:02 -0600

    Added nav menu at top right and help file(s).

--- scripts/editor.js
+++ scripts/editor.js
@@ -756,4 +756,11 @@ $(document).ready(function() {
   $('body').tooltip({
     selector: '[data-toggle="tooltip"]'
   });
- });
+  // Make dropdown menus respond to hover.
+  $('ul.nav li.dropdown').hover(function() {
+    $(this).find('.dropdown-menu').stop(true,
+                                       true).delay(100).fadeIn(100);
+  }, function() {
+    $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeOut(100);
+  });
+});
